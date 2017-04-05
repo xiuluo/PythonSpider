9020478dbdad6409819d338e0bbbdc38db233d4d
Shura <chen919695074@gmail.com>
2017-04-05 13:20:24 +0800

    添加todo列表

--- ScanFreess.py
+++ ScanFreess.py
@@ -10,8 +10,10 @@ configPath = "D:\Soft\Shadowsocks\gui-config.json"
 
 def deCode():
     count = 0
+    #TODO 本地解析二维码
+    # 在线解析二维码接口不稳定，粗暴的重复请求来解决
     while True:
-        r = requests.post('http://cli.im/Api/Browser/deqr', data={'data': 'http://freess.org/images/servers/jp01.png'})
+        r = requests.post('https://cli.im/Api/Browser/deqr', data={'data': 'http://freess.org/images/servers/jp01.png'})
         res = r.json()
         if res["status"] == 1 and count < 10:  # 最多请求十次
             return res
@@ -38,6 +40,7 @@ def modifyConfigJson():
     with open(configPath, 'r') as f:
         ssConfig = json.load(f)
     ssConfigList = ssConfig["configs"]
+    #TODO 自动添加freess而不只是修改
     for data in ssConfigList:
         if data['remarks'] == "freess":
             data['server'] = ssInfo[0]
@@ -50,3 +53,4 @@ def modifyConfigJson():
 
 
 modifyConfigJson()
+#TODO 自动重启ss客户端程序
