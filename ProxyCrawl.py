4049aa5cae8588b9a4d363cf6b600dfcb7a3a882
Shura <chen919695074@gmail.com>
2017-11-02 19:48:59 +0800

    使用高匿代理

--- ProxyCrawl.py
+++ ProxyCrawl.py
@@ -25,7 +25,7 @@ def get_random_ip(ip_list):
 
 
 def get_proxy_ip():
-    proxy_url = 'http://www.xicidaili.com/wt/'
+    proxy_url = 'http://www.xicidaili.com/nn/'
     proxy_headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/62.0.3202.62 Safari/537.36 '
